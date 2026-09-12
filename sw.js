31ee9f6932408d9401f9d106bb8b18bafdc2d058
Aadhav20 <142462795+SriKrishnaSteels@users.noreply.github.com>
2026-09-12 09:34:07 -0400

    Bump CACHE_NAME to sks-cache-v6
    Updated cache name to v6 for new versioning.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sks-cache-v5";
+const CACHE_NAME = "sks-cache-v6";
 
 // Core files needed for the site to still open when offline.
 // Bump CACHE_NAME (e.g. to "sks-cache-v2") whenever you want visitors
